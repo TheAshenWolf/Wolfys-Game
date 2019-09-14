316a5d710bfc91975e47c1fde9b13aa1a7f44b39
Pozitrone <jirirkas.skola@gmail.com>
2019-09-14 21:55:59 +0200

    Started complete rework. All TS files were deleted and the whole app will be made in pure Javascript. Added basic frontend page, which will receive buttons and other features later on.

--- types/player.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Inventory } from "./inventory.interface";
-import { Stats } from "./stats.interface";
-
-export interface Player{
-    inventory: Inventory,
-    stats: Stats,
-    level: number,
-    xp: number
-}
--- types/world.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface World{
-    seed: number,
-    collectedLootboxes: Array<Coordinates>,
-    createdAt: Date,
-    worldName: string
-}
