65c8a5f382b2f3e518289d8eb1966502ac8cb7b2
Pozitrone <jirirkas.skola@gmail.com>
2019-09-21 12:44:54 +0200

    Enabled logging

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,35 +9,35 @@ import { SOM, SubscriptionObject } from '../som/SubscriptionObject';
 export class GameComponent implements OnInit, OnDestroy {
 
     subscription: SubscriptionObject = {};
+    consoleLog: string = ``;
 
     constructor() { }
 
     ngOnInit(): void {
+        this.log('woof');
+        this.log('woof');
     }
 
     public worldcreation(): void {
 
     }
 
+    public log(message) {
+        let date = new Date();
+        this.consoleLog += `[${date.toLocaleTimeString()}] > ${message} <br>`;
+    }
+
+
     /*
-        var terminal = document.getElementById("log");
-            var command = document.getElementById("command-line");
-            var runButton = document.getElementById("run-button");
-    
-            var createWorldButton = document.getElementById("create-world");
-            createWorldButton.onclick = (() => {
-                getWorldData();
-                
-            });
-    
+               
             function getWorldData() {
-                log("Please, write the world name:");
+                log('Please, write the world name:');
                 runButton.onclick = (() => {
-                    if (command.value == null || command.value == "") {
-                        log("World name mustn't be empty!");
+                    if (command.value == null || command.value == '') {
+                        log('World name mustn't be empty!');
                     }
                     else {
-                        log("Set name: " + command.value);
+                        log('Set name: ' + command.value);
                         resetCommand();
     
                         let name = command.value;
@@ -47,30 +47,26 @@ export class GameComponent implements OnInit, OnDestroy {
             }
     
             function getWorldSeed(name) {
-                log("Please, write a seed (number), or leave empty for random seed.");
+                log('Please, write a seed (number), or leave empty for random seed.');
                 runButton.onclick = (() => {
                     if (isNaN(Number(command.value))) {
-                        log("Seed must be a number")
+                        log('Seed must be a number')
                     }
-                    else if (command.value == null || command.value == "") {
+                    else if (command.value == null || command.value == '') {
                         let seed = Math.floor(Math.random() * 10000000)
-                        log("Set seed: " + seed);
+                        log('Set seed: ' + seed);
                         resetCommand();
                         worldgen(name, seed);
                     }
                     else {
-                        log("Set seed: " + command.value);
+                        log('Set seed: ' + command.value);
                         resetCommand();
                         worldgen(name, seed);
                     }
                 });
             }
     
-            function log(message) {
-                let date = new Date()
-                command.value = "";
-                terminal.innerHTML += `[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] > ${message} <br>`;
-            }
+            function 
     
             function resetCommand() {
                 runButton.onclick = (() => {return null});
