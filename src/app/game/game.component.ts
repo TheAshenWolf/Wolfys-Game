31b22fd001f625d72565bcb5047e5c7344789296
Pozitrone <jirirkas.skola@gmail.com>
2019-09-22 09:25:58 +0200

    Added the idea from yesterday about seed and worldname acquisition. Also, cleaned up ngOnInit and added random seed option

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,23 +7,43 @@ import { SOM, SubscriptionObject } from '../som/SubscriptionObject';
     styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit, OnDestroy {
-    @ViewChild('command', {static: true}) commandLine;
+    @ViewChild('command', { static: true }) commandLine;
 
     subscription: SubscriptionObject = {};
     consoleLog: string = ``;
+    creatingWorld: boolean = false;
 
     constructor() { }
 
     ngOnInit(): void {
-        this.log('woof');
-        this.log('woof');
     }
 
     public submit() {
         this.log('woof?');
     }
 
-    public async worldcreation(): Promise<void> {
+    public startWorldCreation() {
+        if(!this.creatingWorld){
+            this.log('Please, write in the world name and the seed. The seed may be empty for a random one.');
+        }
+        this.creatingWorld = true;
+    }
+
+    public worldCreation(name, seed, event) {
+        if ((name && name.length > 2)) {
+            if (seed === undefined || seed === null || seed === '') {
+                seed = Math.floor(Math.random() * 10000000);
+            }
+            this.creatingWorld = false;
+            this.log('Started world creation process...');
+            this.log('World Name: ' + name);
+            this.log('World Seed: ' + seed);
+        }
+        else {
+            this.log('World name must be at least 3 characters long!');
+        }
+
+        event.stopPropagation();
 
     }
 
