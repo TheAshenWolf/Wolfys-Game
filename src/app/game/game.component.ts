9e4128f1cfc6aa73ff9ca99d870c6bef9b09947c
Pozitrone <jirirkas.skola@gmail.com>
2019-09-22 09:26:27 +0200

    Removed old JS code as everything from it was implemented

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -52,52 +52,6 @@ export class GameComponent implements OnInit, OnDestroy {
     }
 
 
-    /*
-               
-            function getWorldData() {
-                log('Please, write the world name:');
-                runButton.onclick = (() => {
-                    if (command.value == null || command.value == '') {
-                        log('World name mustn't be empty!');
-                    }
-                    else {
-                        log('Set name: ' + command.value);
-                        resetCommand();
-    
-                        let name = command.value;
-                        getWorldSeed(name);
-                    }
-                });
-            }
-    
-            function getWorldSeed(name) {
-                log('Please, write a seed (number), or leave empty for random seed.');
-                runButton.onclick = (() => {
-                    if (isNaN(Number(command.value))) {
-                        log('Seed must be a number')
-                    }
-                    else if (command.value == null || command.value == '') {
-                        let seed = Math.floor(Math.random() * 10000000)
-                        log('Set seed: ' + seed);
-                        resetCommand();
-                        worldgen(name, seed);
-                    }
-                    else {
-                        log('Set seed: ' + command.value);
-                        resetCommand();
-                        worldgen(name, seed);
-                    }
-                });
-            }
-    
-            function 
-    
-            function resetCommand() {
-                runButton.onclick = (() => {return null});
-            }
-    
-    */
-
     ngOnDestroy(): void {
         SOM.clearSubscriptionsObject(this.subscription);
     }
