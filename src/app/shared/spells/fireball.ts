6e4bd3ee9b054667f160ce214fd3f1f4b1b89b18
Pozitrone <jirirkas.skola@gmail.com>
2019-11-05 09:34:43 +0100

    Added directional fireball #29

--- src/app/shared/spells/fireball.ts
+++ src/app/shared/spells/fireball.ts
@@ -4,6 +4,7 @@ export const fireball = {
     cost: 25,
     damage: 50,
     type: 'projectile',
-    src: environment.assets + 'spells/fireball.png',
-    endSrc: environment.assets + 'spells/fire-splash.png'
+    src: environment.assets + 'spells/fireball-',
+    endSrc: environment.assets + 'spells/fire-splash-',
+    fileType: '.png'
 }
